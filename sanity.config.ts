1d0a190a84e9a8259979d9d1b780fc3779f9c7c6
weiyolan <yolan.weiler@gmail.com>
2024-04-09 21:55:12 +0200

    netlify widget in sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,11 +3,13 @@
  */
 
 import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
+import { defineConfig, isDev } from "sanity";
 // import { defineConfig } from "@sanity-typed/types";
 // import type { InferSchemaValues } from "@sanity-typed/types";
+import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
 import { structureTool } from "sanity/structure";
+import { dashboardTool } from "@sanity/dashboard";
 import { myStructure } from "sanity.structure";
 import { frFRLocale } from "@sanity/locale-fr-fr";
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
@@ -31,23 +33,59 @@ const config = defineConfig({
   // toolMenu: MyToolMenu,
   // }
   // },
-  plugins: [
-    frFRLocale(),
-    table(), // https://www.sanity.io/plugins/sanity-table
-    structureTool({ structure: myStructure }),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
-    iconify({
-      // Optional configuration
-      // Filter icons by collection for all Icon fields (this field has typed autocomplete ✨)
-      // Defaults to empty array (all collections)
-      collections: ["lucide"],
-      // Shows the selected icon name and collection underneath the icon picker
-      // Defaults to false
-      showName: false,
-    }),
-  ],
+
+  plugins: isDev //process.env.NODE_ENV === "development" | "production"
+    ? [
+        visionTool({ defaultApiVersion: apiVersion }),
+        frFRLocale(),
+        table(), // https://www.sanity.io/plugins/sanity-table
+        structureTool({ structure: myStructure }),
+        iconify({
+          // Optional configuration
+          // Filter icons by collection for all Icon fields (this field has typed autocomplete ✨)
+          collections: ["lucide"], // Defaults to empty array (all collections)
+          // Defaults to false
+          showName: false, // Shows the selected icon name and collection underneath the icon picker
+        }),
+        dashboardTool({
+          widgets: [
+            netlifyWidget({
+              title: "Netlify deploy",
+              sites: [
+                {
+                  title: "Nu Website",
+                  apiId: "cedb80f0-961a-4e99-b04a-fbe699e42959",
+                  buildHookId: "66159c34abed712b09aaf25e",
+                  name: "nu-site",
+                },
+              ],
+            }),
+          ],
+        }),
+      ]
+    : [
+        frFRLocale(),
+        table(), // https://www.sanity.io/plugins/sanity-table
+        structureTool({ structure: myStructure }),
+        iconify({
+          collections: ["lucide"],
+        }),
+        dashboardTool({
+          widgets: [
+            netlifyWidget({
+              title: "Netlify deploy",
+              sites: [
+                {
+                  title: "Nu Website",
+                  apiId: "cedb80f0-961a-4e99-b04a-fbe699e42959",
+                  buildHookId: "66159c34abed712b09aaf25e",
+                  name: "nu-site",
+                },
+              ],
+            }),
+          ],
+        }),
+      ],
 });
 
 export default config;
