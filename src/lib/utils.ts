834f28a8ecae72e62e22ce37ee5f00c5b6ba0cca
weiyolan <yolan.weiler@gmail.com>
2024-05-25 10:08:30 +0200

    return url update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,8 @@ export function cn(...inputs: ClassValue[]) {
 export const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
 
 export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"}${path}`;
+  return `${process.env.NEXT_PUBLIC_APP_URL || "https://nu-soins.com"}${path}`;
+  // return `${process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"}${path}`;
 }
 export const timestamps: { createdAt: true; updatedAt: true } = {
   createdAt: true,
